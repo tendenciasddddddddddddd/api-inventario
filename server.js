5fe984e62604dd0285ede74ebe01a2ec540a509b
tendenciasddddddddddddd <10004095632w@gmail.com>
2022-05-23 21:47:08 -0500

    make it bette cors

--- server.js
+++ server.js
@@ -57,7 +57,11 @@ i18n.configure({
 app.use(i18n.init)
 
 // Init all other stuff
-app.use(cors())
+var corsOptions = {
+  origin: '*', // Reemplazar con dominio
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 app.use(passport.initialize())
 app.use(compression())
 app.use(helmet())
